038384c1d1ab3f874ddc2abc407bf319c629afee
xuguoliang <1044748759@qq.com>
2018-11-23 17:48:50 +0800

    filter

--- config/permission.php
+++ config/permission.php
@@ -42,47 +42,47 @@ return [
                     [
                         'label' =>  '用户管理',
                         'items' =>  [
-                            ['url'=>'/setting/user','label'=>'首页'],
-                            ['url'=>'/setting/user/create','label'=>'新增用户'],
-                            ['url'=>'/setting/user/view', 'label'=>'用户授权'],
-                            ['url'=>'/setting/user/delete','label'=>'删除用户'],
-                            ['url'=>'/setting/user/update','label'=>'修改用户'],
-                            ['url'=>'/setting/user/change-name','label'=>'快捷修改用户名'],
-                            ['url'=>'/setting/user/change-time','label'=>'快捷修改创建时间'],
+                            ['url'=>'/admin/user','label'=>'首页'],
+                            ['url'=>'/admin/user/create','label'=>'新增用户'],
+                            ['url'=>'/admin/user/view', 'label'=>'用户授权'],
+                            ['url'=>'/admin/user/delete','label'=>'删除用户'],
+                            ['url'=>'/admin/user/update','label'=>'修改用户'],
+                            ['url'=>'/admin/user/change-name','label'=>'快捷修改用户名'],
+                            ['url'=>'/admin/user/change-time','label'=>'快捷修改创建时间'],
                         ]
                     ],
                     ['label'=>'角色管理','items' => [
-                        ['url'=>'/setting/roles','label'=>'首页'],
-                        ['url'=>'/setting/roles/create','label'=>'添加角色'],
-                        ['url'=>'/setting/roles/view', 'label'=>'修改角色权限'],
-                        ['url'=>'/setting/roles/delete','label'=>'删除角色'],
-                        ['url'=>'/setting/roles/update','label'=>'修改角色名'],
+                        ['url'=>'/admin/roles','label'=>'首页'],
+                        ['url'=>'/admin/roles/create','label'=>'添加角色'],
+                        ['url'=>'/admin/roles/view', 'label'=>'修改角色权限'],
+                        ['url'=>'/admin/roles/delete','label'=>'删除角色'],
+                        ['url'=>'/admin/roles/update','label'=>'修改角色名'],
                     ]],
                     [
                         'label' =>  '权限组管理',
                         'items' =>  [
-                            ['url'=>'/setting/permission','label'=>'首页'],
-                            ['url'=>'/setting/permission/create','label'=>'新增权限组'],
-                            ['url'=>'/setting/permission/view', 'label'=>'修改权限组权限'],
-                            ['url'=>'/setting/permission/delete','label'=>'删除权限组'],
-                            ['url'=>'/setting/permission/update','label'=>'修改权限组名称'],
+                            ['url'=>'/admin/permission','label'=>'首页'],
+                            ['url'=>'/admin/permission/create','label'=>'新增权限组'],
+                            ['url'=>'/admin/permission/view', 'label'=>'修改权限组权限'],
+                            ['url'=>'/admin/permission/delete','label'=>'删除权限组'],
+                            ['url'=>'/admin/permission/update','label'=>'修改权限组名称'],
                         ]
                     ],
                     [
                         'label' =>  '路由管理',
                         'items' =>  [
-                            ['url'=>'/setting/route','label'=>'首页'],
-                            ['url'=>'/setting/route/delete','label'=>'路由删除'],
-                            ['url'=>'/setting/route/update','label'=>'路由修改'],
+                            ['url'=>'/admin/route','label'=>'首页'],
+                            ['url'=>'/admin/route/delete','label'=>'路由删除'],
+                            ['url'=>'/admin/route/update','label'=>'路由修改'],
                         ]
                     ],
                     [
                         'label' =>  '规则管理',
                         'items' =>  [
-                            ['url'=>'/setting/rule','label'=>'首页'],
-                            ['url'=>'/setting/rule/delete','label'=>'删除规则'],
-                            ['url'=>'/setting/rule/update','label'=>'修改规则'],
-                            ['url'=>'/setting/rule/create','label'=>'新增规则'],
+                            ['url'=>'/admin/rule','label'=>'首页'],
+                            ['url'=>'/admin/rule/delete','label'=>'删除规则'],
+                            ['url'=>'/admin/rule/update','label'=>'修改规则'],
+                            ['url'=>'/admin/rule/create','label'=>'新增规则'],
                         ]
                     ]
                 ]
